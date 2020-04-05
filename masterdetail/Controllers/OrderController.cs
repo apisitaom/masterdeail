28eded576ea85f994973e0c5d08bf5472dd00a11
apisitprompha <apisitprompha@gmail.com>
2020-04-06 05:28:23 +0700

    Add project files.

---
+++ masterdetail/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using masterdetail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace masterdetail.Controllers
+{
+    public class OrderController : Controller
+    {
+        OnlineShopEntities db = new OnlineShopEntities();
+        public ActionResult Index()
+        {
+            List<Customer> OrderAndCustomerList = db.Customers.ToList(); 
+            return View(OrderAndCustomerList);
+        }
+    }
+}
