f6df82c3e7df3c7c641c2bb4082a6cfed7fd8f34
apisitprompha <apisitprompha@gmail.com>
2020-04-06 13:53:05 +0700

    FINISH order and customer

--- masterdetail/Controllers/OrderController.cs
+++ masterdetail/Controllers/OrderController.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace masterdetail.Controllers
@@ -12,8 +11,36 @@ namespace masterdetail.Controllers
         OnlineShopEntities db = new OnlineShopEntities();
         public ActionResult Index()
         {
-            List<Customer> OrderAndCustomerList = db.Customers.ToList(); 
+            List<Customer> OrderAndCustomerList = db.Customers.ToList();
+
             return View(OrderAndCustomerList);
+        }
+        public ActionResult saveOrder(string name, string address, Order[] order) {
+            string result = "Error! Order is not complete!!!";
+            if (name != null || address != null || order != null) {
+                var customerId = Guid.NewGuid();
+                Customer model = new Customer();
+                model.CustomerId = customerId;
+                model.Name = name;
+                model.Address = address;
+                model.OrderDate = DateTime.Now;
+                db.Customers.Add(model);
+
+                foreach (var item in order) {
+                    var orderId = Guid.NewGuid();
+                    Order O = new Order();
+                    O.OrderId = orderId;
+                    O.ProductName = item.ProductName;
+                    O.Quantity = item.Quantity;
+                    O.Price = item.Price;
+                    O.Amount = item.Amount;
+                    O.CustomerId = customerId;
+                    db.Orders.Add(O);
+                }
+                db.SaveChanges();
+                result = "SUCCESS FOR ORDER ^_^";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
